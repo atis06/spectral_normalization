c7eb5e882776fd42cd2e788da4fe8ed64feb8e64
Attila Kántor <attila.kantor@ericsson.com>
2018-10-08 14:49:30 +0200

    Prototype

--- spectral_normalization.py
+++ spectral_normalization.py
@@ -2,40 +2,42 @@ from __future__ import print_function
 
 import numpy as np
 
-matrix = np.array([[-4, -33, -2],
-       [-1,  0,  1],
+matrix = np.array([[4, 33, 2],
+       [1,  0,  1],
        [ 2,  3,  14]])
-def l2_normalize(matrix):
-	return matrix / np.linalg.norm(matrix)
 
-	   
+
+
+
 class Spectral_Normalization:
-	def __init__(self):
-		self._u = np.ndarray(shape=3)
-		
-	def initialize_vector(self):
-		height = matrix.shape[0]
-		width = matrix.shape[1]
+    def __init__(self, matrix, iterations):
+        self.iterations = iterations
+        self.matrix = matrix
+        self.height = matrix.shape[0]
+        self.width = matrix.shape[1]
+        self._u = np.ndarray(shape = self.height)
 
-		self._u = l2_normalize(np.random.normal(0, 1,(height)))
+    def l2_normalize(self, matrix):
+        return matrix / np.linalg.norm(matrix)
 
-	def _update_u_v(self):
-		self.initialize_vector()
-		w = matrix
-		u = self._u
-		
-		height = w.shape[0]
-		for _ in range(10):
-			v = l2_normalize(w.dot(u))
-			u = l2_normalize(w.dot(v))
-			print("v: ")
-			print(v)
-			print("u: ")
-			print(u)
+    def initialize_vector(self):
+        self._u = self.l2_normalize(np.random.normal(0, 1,(self.height)))
 
-		self._u = u
-		w = w / u.dot(w.dot(v))
-		print(w)
-		
-s = Spectral_Normalization()
+    def _update_u_v(self):
+        self.initialize_vector()
+        w = matrix
+        u = self._u
+        print("In round 0. u is: %s" % (u))
+        height = w.shape[0]
+        for i in range(self.iterations):
+            v = self.l2_normalize(w.dot(u))
+            u = self.l2_normalize(w.dot(v))
+            print("In round %s. v is: %s u is: %s" %(i+1,v,u))
+
+        self._u = u
+        w = w / u.dot(w.dot(v))
+        print("\nOriginal matrix is: \n %s" % (self.matrix))
+        print("\nUpdated matrix is: \n %s" %(w))
+
+s = Spectral_Normalization(matrix, 110)
 s._update_u_v()
